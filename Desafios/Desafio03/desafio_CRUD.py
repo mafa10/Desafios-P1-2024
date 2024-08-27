b63ca4a72e8f5b2da8eca2102a0e4721841059ee
mafa10 <mafa@uade.edu.ar>
2024-08-27 00:53:38 -0300

    Pido los datos del libro en la función 'agregar_libro'

---
+++ Desafios/Desafio03/desafio_CRUD.py
@@ -0,0 +1,27 @@
+"""Hacer un CRUD, mi caso va a ser un CRUD de Libros"""
+
+"""Campos del Libro: ID, TITULO, AUTOR, EDITORIAL, AÑO PUBLI, PAGINAS"""
+
+"""La idea sería pedir los datos del libro por teclado, e ir cargando libro a libro, después, mostrarlos por pantalla en una matriz, poder agregar más, modificar los datos de un libro y poder eliminar un libro."""
+
+def agregar_libro():
+    libros = []
+    titulo = input("Ingrese el Titulo del Libro:").title()
+    autor = input("Ingrese el Autor del Libro:").title()
+    editorial = input("Ingrese la Editorial del Libro:").title()
+    anio = input("Ingrese el Año de Publicación del Libro:")
+    cantPag = input("Ingrese la Cantidad de Páginas del Libro:")
+    pass
+
+def eliminar_libro():
+    pass
+
+def modificar_libro():
+    pass
+
+def imprimir_libros():
+    pass
+
+#Programa Principal
+
+
