ad56ff3473fec83058eee28d13bf8380f889f20b
mafa10 <mafa@uade.edu.ar>
2024-09-02 20:30:44 -0300

    pruebas para imprimir los libros

--- Desafios/Desafio03/desafio_CRUD.py
+++ Desafios/Desafio03/desafio_CRUD.py
@@ -5,14 +5,28 @@
 """La idea sería pedir los datos del libro por teclado, e ir cargando libro a libro, después, mostrarlos por pantalla en una matriz, poder agregar más, modificar los datos de un libro y poder eliminar un libro."""
 
 def agregar_libro(lib):
-    #Pensar mejor la validacion de los campos.
-    #Hacer una funcion aparte para validar cada campo.
+    """
+    Recibe los datos de un libro y valida la entrada con funciones.
+    Crea un diccionario y lo agrega a una lista para almacenarlos.
+    """
+    id = input("Ingrese un Id:")
     titulo = input("Ingrese el Titulo del Libro:").strip().title()
     autor = validar_autor(input("Ingrese el Nombre del Autor del Libro:"))
     editorial = input("Ingrese la Editorial del Libro:").title()
     anio = validar_anio(input("Ingrese el Año de Publicación del Libro:").strip())
     cantPag = validar_cantPag(input("Ingrese la Cantidad de Páginas del Libro:").strip())
-    libro = [titulo, autor.title().strip(), editorial, anio, cantPag]
+    
+    # Uso de Diccionario
+    libro = {
+        'Id' : id,
+        'Titulo' : titulo,
+        'Autor'  : autor,
+        'Editorial' : editorial,
+        'Anio' : anio,
+        'CantPag' : cantPag
+    }
+
+    # lib = Lista de Diccionarios
     lib.append(libro)
     return lib
 
