3dd1ea94843606c6766b85f73ac9d7e9aaa0351f
mafa10 <mafa@uade.edu.ar>
2024-09-11 03:56:11 -0300

    Funcion val_email terminada

--- Desafios/Desafio05/desafio_expRegular.py
+++ Desafios/Desafio05/desafio_expRegular.py
@@ -1,5 +1,12 @@
-def val_email():
-    pass
+import re
+
+def val_email(e):
+    """Solo se permiten letras (a-z), números (0-9), puntos (.) y caracteres espciales como '_, %, +, -'."""
+    patron = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    while not re.match(patron, e):
+        print("El email ingresado es inválido. Intente nuevamente.")
+        e = input("Ingrese un Email: ")
+    print("El email ingresado es válido")
 
 def val_num_tel():
     pass
@@ -11,3 +18,7 @@ def val_fecha():
     pass
 
 #Programa Principal
+import re
+email = val_email(input("Ingrese un Email: "))
+"[a-z]" #El guion define un rango de caracteres
+"[0-9]"
