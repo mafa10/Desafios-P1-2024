135e8d2c4720faf25e8a917d3be873f5c5acddb4
mafa10 <mafa@uade.edu.ar>
2024-09-12 21:20:39 -0300

    Complete la función val_user y termine el desafio

--- Desafios/Desafio05/desafio_expRegular.py
+++ Desafios/Desafio05/desafio_expRegular.py
@@ -6,7 +6,7 @@ def val_email(e):
     while not re.match(patron, e):
         print("El email ingresado es inválido. Intente nuevamente.")
         e = input("Ingrese un Email: ")
-    print("El email ingresado es válido")
+    print("El email ingresado es válido.")
 
 def val_num_tel(tel):
     """
@@ -28,34 +28,41 @@ def val_num_tel(tel):
     """
     patron = '^\+?(\d{1,3})?\s?-?(\d{2,4})\s?-?(\d{2,4})\s?-?(\d{2,4})$'
     while not re.match(patron, tel):
-        print("No se encontro el número de teléfono")
+        print("No se encontro el número de teléfono.")
         tel = input("Marque nuevamente: ")
-    print("Teléfono encontrado")
+    print("Teléfono encontrado.")
 
 def val_cod_post(cod):
     patron = '^([A-Za-z]\d{4}[A-Za-z]{3})' # En Arg.
     while not re.match(patron, cod):
-        print("Código postal inválido")
+        print("Código postal inválido.")
         cod = input("Reingrese un código postal: ")
-    print("Código postal válido")
+    print("Código postal válido.")
 
 def val_fecha(date):
     """Formato DD/MM/AAAA"""
     patron = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/(\d{1,4})$'
     while not re.match(patron, date):
-        print("Fecha inválida")
+        print("Fecha inválida.")
         date = input("Reingrese la fecha (DD/MM/AAAA): ")
-    print("Fecha válida")
+    print("Fecha válida.")
 
 def val_user(usr):
+    """Validación para un usuario con al menos 3 caracteres y hasta 16."""
     patron = '^[a-zA-Z0-9._-]{3,16}$'
+    #el patron permite letras min y mayus, números,puntos y guiones entre 3 y 16 caracteres.
+    while not re.match(patron, usr):
+        print("Nombre de usuario inválido")
+        usr = input("Ingrese un nombre de usuario (entre 3 y 16 caracteres): ")
+    print("Nombre de usuario válido.")
     pass
 
 def __main__():
-    #email = val_email(input("Ingrese un Email: "))
-    #telefono = val_num_tel(input("Ingrese un número de teléfono a buscar: "))
-    #codPostal = val_cod_post(input("Ingrese un código postal: "))
+    email = val_email(input("Ingrese un Email: "))
+    telefono = val_num_tel(input("Ingrese un número de teléfono a buscar: "))
+    codPostal = val_cod_post(input("Ingrese un código postal: "))
     fecha = val_fecha(input("Ingrese una fecha (DD/MM/AAAA): "))
+    usuario = val_user(input("Ingrese un nombre de usuario (entre 3 y 16 caracteres): "))
     #help(val_num_tel)
 
 #Programa Principal
